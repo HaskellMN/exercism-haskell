e0e367ad194b4cf4becde66ae0764e8b1dae6716
aberg <aberg@tamarackconsulting.com>
2019-07-16 20:26:07 -0500

    Poker

--- exercises/poker/src/Poker.hs
+++ exercises/poker/src/Poker.hs
@@ -3,6 +3,8 @@ module Poker (bestHands) where
 import Data.List.Split (splitOn)
 
 data Suit = Hearts | Spades | Diamonds | Clubs deriving (Eq)
+instance Ord Suit where
+  compare _ _ = EQ
 
 instance Show Suit where
   show Hearts = "H"
@@ -46,6 +48,11 @@ data Card = Card {
   suit :: Suit
 }
 
+instance Eq Card where
+  (Card f1 _) == (Card f2 _) = f1 == f2
+instance Ord Card where
+  compare (Card f1 _) (Card f2 _) = compare f1 f2
+
 instance Show Card where
   show (Card f s) = (show f) ++ (show s)
 
@@ -54,8 +61,11 @@ data Hand = Hand Card Card Card Card Card
 instance Show Hand where
   show (Hand a b c d e) = (show a) ++ " " ++ (show b) ++ " " ++ (show c) ++ " " ++ (show d) ++ " " ++ (show e)
 
+-- GOAL Function
 bestHands :: [String] -> Maybe [String]
 bestHands hands = Just hands
+-- bestHands hands = parseHand hands
+-- GOAL Function
 
 parseSuit :: Char -> Maybe Suit
 parseSuit 'H' = Just Hearts
@@ -88,30 +98,43 @@ parseCard _ = Nothing
 parseHand :: String -> Maybe Hand
 parseHand s = do
   x <- sequence $ parseCard <$> splitOn " " s
+  -- x :: List Card <- (sequence $ (parseCard <$> (splitOn " " s :: List String) :: List (Maybe Card)) :: Maybe (List Card))
   toHand x
   where
     toHand (a : b : c : d : e : []) = Just $ Hand a b c d e
     toHand _ = Nothing
 
-data HandRank
-  = StraightFlush FaceValue
-  | FourOfAKind FaceValue
-  | FullHouse FaceValue
-  | Flush Hand
-  | Straight FaceValue
-  | ThreeOfAKind FaceValue
-  | TwoPair FaceValue FaceValue FaceValue
-  | Pair FaceValue FaceValue FaceValue FaceValue
-  | HighCard Hand
-  deriving (Show)
+--isStraight :: Hand -> Boolean
+--isStraight hand@(Hand c1 c2 c3 c4 c5) =
+--  sortedHand
+--  where
+--  sortedHand = sort $ handToList hand
 
 handToList :: Hand -> [Card]
 handToList (Hand a b c d e) = [a, b, c, d, e]
 
-filterFlush :: Hand -> Maybe HandRank
-filterFlush h@(Hand (Card _ a) (Card _ b) (Card _ c) (Card _ d) (Card _ e)) = if (all (== a) [b,c,d,e])
-  then Just (Flush h)
-  else Nothing
+
+--  if c1 > c2 && c2 > c3 && c3 > c4 && c4 > c5
+--    then true
+--    else false
+
+-- data HandRank
+--   -- = FiveOfAKind FaceValue -- No jokers, so not possible
+--   = StraightFlush FaceValue
+--   | FourOfAKind FaceValue
+--   | FullHouse FaceValue
+--   | Flush Hand
+--   | Straight FaceValue
+--   | ThreeOfAKind FaceValue
+--   | TwoPair FaceValue FaceValue FaceValue
+--   | Pair FaceValue FaceValue FaceValue FaceValue
+--   | HighCard Hand
+--   deriving (Show)
+
+--filterFlush :: Hand -> Maybe HandRank
+--filterFlush h@(Hand (Card _ a) (Card _ b) (Card _ c) (Card _ d) (Card _ e)) = if (all (== a) [b,c,d,e])
+--  then Just (Flush h)
+--  else Nothing
 
 -- filterStraight :: Hand -> Maybe HandRank
 -- filterStraight hand = do
